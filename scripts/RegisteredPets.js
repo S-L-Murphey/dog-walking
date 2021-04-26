dee75af1c821f79c7a1ad6813710e87024c7e9f2
Sean Murphey <smurphey1990@gmail.com>
2021-04-26 14:55:11 -0500

    Completed Who's Walking Whom? Exercise

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -3,34 +3,35 @@ import { getPets, getWalkers } from "./database.js"
 
 
 const pets = getPets()
-const walkers = getWalkers()
+
+
 
 document.addEventListener(
     "click",
     (clickEvent) => {
-
         const itemClicked = clickEvent.target
-
         if (itemClicked.id.startsWith("pet")) {
             const [, petId] = itemClicked.id.split("--")
+            const petIdNumber = parseInt(petId)
 
-            for (const pet of pets) {
+            const walkers = getWalkers()
 
-                if (pet.id == parseInt(petId)) {
-                    
-                    const getPetWalkers = walkers.filter(walker => walker.petId === pet.id);
-
-                    const foundWalkerName = getPetWalkers.find(item => item.name === wa)
-                    
-                    //const found = getPetWalkers.find(element => element.name )
-
-                    window.alert(`${pet.name} is being walked by ${foundWalkerName.name}`)
+            const foundPet = pets.find(
+                (pet) => {
+                    return pet.id === petIdNumber
                 }
-            }
+            )
+
+            const foundWalker = walkers.find(
+                (walker) => {
+                    return walker.id === foundPet.walkerId;
+                }
+            )
+            window.alert(`${foundPet.name} is being walked by ${foundWalker.name}`)
         }
     }
-)
 
+)
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
